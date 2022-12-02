375fc1a6b1d260e6a2efe0be4a7e72eacd0b52f5
Liova <liovaswagg@hotmail.com>
2022-12-02 14:17:22 +0100

    bme280

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
  * @file main.cpp
  * @brief Basic Mbed 6 template file for SNIR students
  * @author Liova
- * @version 1.01
+ * @version 1.0
  * @date 01/12/2022
  */
 
@@ -11,11 +11,10 @@
 
 #define SLEEP_TIME 500ms // (msec)
 BME280 capteur (I2C_SDA, I2C_SCL);
-void initialize(void);
-float getTemperature(void);
+
 
 int main() {
   while (true) {
-    printf("Temperature : %2.2f \r\n", capteur.getTemperature()); 
+    printf("Temperature : %2.2f°C  Pression : %2.2fhPa  Humidité : %2.2f% \r\n", capteur.getTemperature(), capteur.getPressure(), capteur.getHumidity());
   }
 }
