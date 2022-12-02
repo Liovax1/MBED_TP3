b17446d7a37028893ebe8247bcb0d564b7640260
Liova <liovaswagg@hotmail.com>
2022-12-02 15:41:43 +0100

    ecranLCD

--- main.cpp
+++ main.cpp
@@ -8,13 +8,26 @@
 
 #include "mbed.h"
 #include "BME280.h"
+#include "Grove_LCD_RGB_Backlight.h"
 
 #define SLEEP_TIME 500ms // (msec)
 BME280 capteur (I2C_SDA, I2C_SCL);
+Grove_LCD_RGB_Backlight afficheur (I2C_SDA, I2C_SCL);
 
 
 int main() {
   while (true) {
     printf("Temperature : %2.2f°C  Pression : %2.2fhPa  Humidité : %2.2f% \r\n", capteur.getTemperature(), capteur.getPressure(), capteur.getHumidity());
+    afficheur.setRGB(0x255, 0x000, 0x000);
+    afficheur.print("Hello World");
+    afficheur.locate(0,1);
+    afficheur.print("Ceci est un test");
+    afficheur.locate(0,2);
+    ThisThread::sleep_for(SLEEP_TIME);
+    afficheur.setRGB(0x000, 0x255, 0x000);
+    ThisThread::sleep_for(SLEEP_TIME);
+    afficheur.setRGB(0x000, 0x000, 0x255);
+    ThisThread::sleep_for(SLEEP_TIME);
+
   }
 }
